c8a1a2cf22af6ceff25d6c3fa0ee2c1fb754c5ec
IKI <mac@macs-MacBook-Pro.local>
2026-02-11 20:50:06 +0700

    Update maps, address, and tiktok url

--- src/data/brand.ts
+++ src/data/brand.ts
@@ -15,14 +15,14 @@ export const brand = {
   instagramHandle: "@matcha.chii__",
 
   // Sosial media lain
-  tiktokUrl: "https://tiktok.com/@matchachii",
+  tiktokUrl: "https://tiktok.com/@matcha.chii__",
 
   // Alamat toko
-  address: "Jl. Pinang IV. 8C, Kelurahan Pondok Labu, Kecamatan Cilandak, Jakarta Selatan 12450",
+  address: "Jl. Petogogan II No. 41, RT 008/RW 006, Pulo, Kecamatan Kebayoran Baru, Kota Administrasi Jakarta Selatan 12160",
 
   // Google Maps embed
   googleMapsEmbedUrl:
-    "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d247.85369325103716!2d106.79926411854713!3d-6.3084401729175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1770745068387!5m2!1sen!2sid",
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.87931585482875!2d106.79488072661917!3d-6.254633039099008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f174affc6d57%3A0x45dbf67ff6e90f20!2sJl.%20Petogogan%20II%20No.41%2C%20RT.8%2FRW.6%2C%20Pulo%2C%20Kec.%20Kby.%20Baru%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2012160!5e0!3m2!1sen!2sid!4v1770816805296!5m2!1sen!2sid",
 
   // URL menu (opsional)
   menuUrl: "",
